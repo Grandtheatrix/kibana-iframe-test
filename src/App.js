1196fcb979f25a065aa05341ba9b20209833f8de
Robert Webber <rwebber@tensoriot.com>
2021-05-10 16:26:36 -0500

    direct to proxy server

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-     <iframe src="https://search-long-ru-elasti-qcb6r2dze0ci-37hfbmnxxc4fkszky5yowia2sy.eu-west-1.es.amazonaws.com/_plugin/kibana/" height="500" width="1000"/>
+     <iframe src="http://54.247.48.35/" height="500" width="1000"/>
       </header>
     </div>
   );
